7fc8a7eedba6788a9170d652bf6ffee1df33f221
zxhoper <zxhoper@proton.me>
2023-07-05 09:50:32 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// Project: /home/hbc/go/src/go_study/go-echo-simple-webapp/echo_basic-001_2023-07-05
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
